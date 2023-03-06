ae8c0da5a0e020ad4c50e2efb809dd1297177a9d
ndeta <ndetainnocent3@gmail.com>
2023-03-06 15:27:25 +0200

    Adding postgresql

--- src/main/java/com/example/spring01/model/Content.java
+++ src/main/java/com/example/spring01/model/Content.java
@@ -1,10 +1,18 @@
 package com.example.spring01.model;
 
 
+import jakarta.validation.constraints.NotBlank;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-public  record Content(Integer id, String title, String desc,
-                       Status status, Type contentType,
-                       LocalDateTime dateCreated, LocalDateTime dateUpdated, String url) {
+public  record Content(Integer id,
+                       @NotBlank
+                       String title,
+                       String desc,
+                       Status status,
+                       Type contentType,
+                       LocalDateTime dateCreated,
+                       LocalDateTime dateUpdated,
+                       String url) {
 }
